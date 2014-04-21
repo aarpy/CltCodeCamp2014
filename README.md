60d9dcfb830d4dc6c754e19c389ba17771a24ce7
Aarpy <ramu@rohilabs.com>
2014-04-20 20:50:35 -0400

    Added main screen controls with responsive ui

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-CltCodeCamp2014
-===============
+Cloud Architecture Patterns
+===========================
 
-Code for CLT CodeCamp Cloud Architecture Patterns presentation on May 3rd 2014
+Demo for Charlotte Carolina Code Camp 2014 on May 3rd 2014
+
+[Presentation overview](http://codecamp.developersguild.org/sessions/2014/4/3/cloud-architecture-design-patterns-part-2)
+
+[Slides](http://codecamp.developersguild.org/sessions/2014/4/3/cloud-architecture-design-patterns-part-2)
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,39 @@
 
 angular.module('cltCodeCamp2014App')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+
+    $scope.newmessage = "";
+    $scope.username = "username";
+    $scope.servername = "localhost";
+    $scope.messages = [
+      { type: "join", user: "Ramu"},
+      { type: "left", user: "Ramu"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "join", user: "Ramu"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "left", user: "Ramu"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "comment", user: "Ramu", content: "Hello Code Camp!"},
+      { type: "left", user: "Ramu"},
+      { type: "comment", user: "Ramu", content: "Hello RDA!"}
+    ];
+
+    $scope.sendMessage = function() {
+      console.log("send message called: " + $scope.newmessage);
+    };
+
+    $scope.sendHelloCodeCamp = function() {
+      console.log("sendHelloCodeCamp called");
+    };
+
+    $scope.sendRandomQuote = function() {
+      console.log("sendRandomQuote called");
+    };
   });
