08e861a9bfbc3677577e99c19eae07abd5a6f9e7
Aarpy <ramu@rohilabs.com>
2014-04-28 02:48:44 -0400

    Added server names, screen loading UI and CSS changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,22 +5,26 @@ var app = angular.module('cltCodeCamp2014App');
 app.controller('MainCtrl', function ($scope, $http, toastr, socket) {
   // Constants
   var MAX_MESSAGES = 50;
+  // Quotes from https://www.pinterest.com/sbletnitsky/random-sayings-quotes/
   var RANDOM_QUOTES = [
-    'Quote1',
-    'Quote2',
-    'Quote3',
-    'Quote4',
-    'Quote5',
-    'Quote6',
-    'Quote7',
-    'Quote8',
+    'I Love Charlotte',
+    'A moment of patience in a moment of anget saves you a hundred moments of regret',
+    'If you cant be kind, be quiet!',
+    'A few nice words can help a person a lot more than you think',
+    'A book is a device to ignite the imagination',
+    'Your attitude determines your direction',
+    'Caffeine is foundation of my food pyramid',
+    'Make it simple but significant',
   ];
 
   // Initialization
+  $scope.connected = false;
   $scope.newmessage = '';
   $scope.usercount = 1;
   $scope.username = 'Guest';
   $scope.servername = '...';
+  $scope.servernames = [];
+  $scope.servernamesDisplay = '';
   $scope.messages = [];
 
   // Socket implementation
@@ -28,6 +32,10 @@ app.controller('MainCtrl', function ($scope, $http, toastr, socket) {
     $scope.username = data.username;
     $scope.usercount  = data.usercount;
     $scope.servername = data.servername;
+    $scope.servernames = data.servernames;
+    $scope.servernamesDisplay = $scope.servernames.join(', ');
+
+    $scope.connected = true;
   });
 
   socket.on('join', function(data) {
@@ -42,6 +50,15 @@ app.controller('MainCtrl', function ($scope, $http, toastr, socket) {
   socket.on('left', function(data) {
     $scope.usercount  = data.usercount;
     addMessage({ type: 'left', username: data.username });
+  });
+
+  socket.on('server:join', function(data) {
+    $scope.servernames.push(data.servername);
+    $scope.servernamesDisplay = $scope.servernames.join(', ');
+    toastr.info(data.servername + ' server added', 'Information', {
+      'timeOut': '30000',
+      'positionClass': 'toast-bottom-right'
+    });
   });
 
   // Angular event handlers
@@ -72,16 +89,18 @@ app.controller('MainCtrl', function ($scope, $http, toastr, socket) {
   function sendMessage(content) {
     if (content === null || content.length === 0) {
       toastr.error('Enter a message to send', 'Error', {
-        "timeOut": "1000",
-        "positionClass": "toast-bottom-right"
+        'timeOut': '1000',
+        'positionClass': 'toast-bottom-right'
       });
       return;
     }
+    content = content.replace(/[^\w\s\.!,]/gi, '');
+
     console.log('sendMessage: ' + content);
     socket.emit('message', { content: content });
     toastr.info('Your message is queued', 'Information', {
-      "timeOut": "1000",
-      "positionClass": "toast-bottom-right"
+      'timeOut': '1000',
+      'positionClass': 'toast-bottom-right'
     });
   }
 });
