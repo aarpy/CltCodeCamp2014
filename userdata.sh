8a0f9474f84eb3eed60348a4175786a338d49fb6
Aarpy <ramu@rohilabs.com>
2014-05-01 04:21:30 -0400

    Added server down notification

--- userdata.sh
+++ userdata.sh
@@ -3,3 +3,5 @@ export REDIS_HOST_OVERRIDE=172.31.26.231
 cd /home/ubuntu/Workspace/CltCodeCamp2014
 sudo git pull --rebase
 sudo service CltCodeCamp2014 restart
+cp upstart_CltCodeCamp2014Notify.conf /etc/inti/CltCodeCamp2014Notify.conf
+sudo service CltCodeCamp2014Notify restart
